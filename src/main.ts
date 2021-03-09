fe6e0f29ef9adc531e2629b37b616be1da3ef564
Cll <2642137848@qq.com>
2021-03-09 14:38:59 +0800

    添加一些基本的兼容

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style/reset.scss'
 import 'vant/lib/index.css'; // 全局引入样式
+import './style/common.scss'
 import './rem'
 import Vant from 'vant';
 // 挂载路由
